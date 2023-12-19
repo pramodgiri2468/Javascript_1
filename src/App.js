739717c02267be4f268f08db49d480e5cccee618
Roshan Shrestha <shrtroshan289@gmail.com>
2023-12-19 07:22:30 +0545

    friday class

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleChange = (e) => {
+    const new_value = e.target.value;
+    setValue(new_value);
+  };
+
+  const addTodo = () => {
+    // Check if the input value is not empty before adding it to todos
+    if (value) {
+      setTodos([...todos, value]);
+      setValue(""); // Clear the input field after adding a todo
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <h1 className='heading'>To do list</h1>
+        <div className='input-container'>
+          <input type='text' value={value} onChange={handleChange} />
+          <button onClick={addTodo}>Add Todo</button>
+        </div>
+        <div>
+          <ol>
+            {todos.map((todo, index) => {
+              return <li key={index}>{todo}</li>;
+            })}
+          </ol>
+        </div>
+      </div>
     </div>
   );
 }
